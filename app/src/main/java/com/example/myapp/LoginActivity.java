b0abf6e74336f3bc3c505f25fd7988b97cdda93e
lich0706 <lich0706@uw.edu>
2021-06-06 18:35:46 -0400

    Firebase

--- app/src/main/java/com/example/myapp/LoginActivity.java
+++ app/src/main/java/com/example/myapp/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.example.myapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +33,10 @@ public class LoginActivity extends AppCompatActivity {
     private EditText ePassword;
     private Button eLogin;
     private FirebaseAuth mAuth;
+    private String uuID;
+    private static final String TAG = "MyDebugTag";
+    private FirebaseFirestore mStore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +47,9 @@ public class LoginActivity extends AppCompatActivity {
         ePassword = findViewById(R.id.password);
         eLogin = findViewById(R.id.btnLogin);
         mAuth = FirebaseAuth.getInstance();
+        uuID = Settings.Secure.getString(getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        mStore = FirebaseFirestore.getInstance();
 
         eLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,12 +57,35 @@ public class LoginActivity extends AppCompatActivity {
                 String tEmail = eName.getText().toString();
                 String tPassword = ePassword.getText().toString();
 
-                loginUser(tEmail, tPassword);
+                mAuth.signInWithEmailAndPassword(tEmail, tPassword)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    String userID = mAuth.getCurrentUser().getEmail();
+                                    HashMap<String, String> map = new HashMap<>();
+                                    map.put("UUID", uuID);
+                                    DocumentReference dr = mStore.collection("users").document(userID);
+                                    dr.set(map)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Toast.makeText(LoginActivity.this, "Device Saved", Toast.LENGTH_SHORT).show();
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                } else {
+                                    Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
-    }
-
-    private void loginUser(String email, String password) {
-
     }
 }
--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.myapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.Settings;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button signout;
+    private CountDownTimer timer;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private String uuid;
+    private static final String TAG = "MyDebugTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +36,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         signout = findViewById(R.id.btn_Logout);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        uuid = Settings.Secure.getString(getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+
+        getLoginStatus();
 
         signout.setOnClickListener((v) -> {
             FirebaseAuth.getInstance().signOut();
             Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
         });
+    }
+
+    private void getLoginStatus() {
+        timer = new CountDownTimer(30000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.d(TAG, "onTick: " + 1/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                timer.start();
+                String userID = mAuth.getCurrentUser().getEmail();
+                FirebaseFirestore.getInstance().collection("users")
+                        .document(userID)
+                        .get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot snap) {
+                                String uniqueID = snap.get("UUID").toString();
+                                if (!uniqueID.equals(uuid)) {
+                                    FirebaseAuth.getInstance().signOut();
+                                    Toast.makeText(MainActivity.this, "Multiple Device Detected", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        }.start();
     }
 
 }
